37824e6d03697d99bd0b96bdbe952de229aacbe4
Patrick Patchelli <patrick.patchelli@hotmail.com>
2022-08-08 13:04:37 -0300

    create footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Contact from "../Components/Contact/Contact";
+import Footer from "../Components/Footer/Footer";
 import Intro from "../Components/Intro/Intro";
 import Navbar from "../Components/Navbar/Navbar";
 import Product from "../Components/Products/Product";
@@ -18,6 +19,7 @@ const Home = () => {
             <Product />
             <Testimonial />
             <Contact />
+            <Footer />
         </>
     )
 }
