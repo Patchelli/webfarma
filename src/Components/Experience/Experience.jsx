482992b10d722c97bd58eabb194486cafbf78a8f
Patrick Patchelli <patrick.patchelli@hotmail.com>
2022-08-08 13:18:11 -0300

    created experience

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Experience.css";
+const Experience = () => {
+
+
+    return (
+        <div className="experience" id='experience'>
+            <div className="achievement">
+                <div className="circle">8+</div>
+                <span >anos </span>
+                <span>Experiência</span>
+            </div>
+            <div className="achievement">
+                <div className="circle" >1000+</div>
+                <span >entregas </span>
+                <span>Completas</span>
+            </div>
+            <div className="achievement">
+                <div className="circle" >5+</div>
+                <span >Empresas </span>
+                <span>Parceiras</span>
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
